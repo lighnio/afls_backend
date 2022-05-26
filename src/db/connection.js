95e4b85c5d3a1cac74c7b87b63626de22051e408
lighnio <charlesvas16@outlook.com>
2022-05-26 11:23:35 -0600

    clients methods ready, GET, GET One, POST, PUT and DELETE

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,61 @@
 const sql = require("mssql");
 const config = require("./config");
 
+// Administradores
 const getAdmins = async () => {
   const pool = await sql.connect(config);
-  const admins = pool.request().query("SELECT * FROM Administrador");
+  const admins = pool.request()
+  .execute('GetAdministradores')
   return admins;
 };
 
+
+// Clientes
+
 const getClients = async () => {
   const pool = await sql.connect(config);
-  const clients = pool.request().query("SELECT * FROM Cliente");
+  const clients = pool.request()
+  .execute('GetClientes');
   return clients;
+};
+
+const getClient = async (id) => {
+  const pool = await sql.connect(config);
+  const clients = pool.request()
+  .input('IdCliente', sql.Int, id)
+  .execute('GetCliente');
+  return clients;
+};
+
+const insClient = async (cliente) => {
+  const pool = await sql.connect(config);
+  const client = pool.request()
+  .input('Nombre', sql.VarChar(12), cliente.Nombre)
+  .input('Apellido', sql.VarChar(15), cliente.Apellido)
+  .input('NoTel', sql.VarChar(20), cliente.NoTel)
+  .input('NoDpi', sql.VarChar(20), cliente.NoDpi)
+  .execute('InsCliente');
+  return client;
+};
+
+const delClient = async (id) => {
+  const pool = await sql.connect(config);
+  const client = pool.request()
+  .input('IdCliente', sql.Int, id)
+  .execute('DelCliente');
+  return client;
+};
+
+const updClient = async (id, cliente) => {
+  const pool = await sql.connect(config);
+  const client = pool.request()
+  .input("IdCliente", sql.Int, id)
+  .input('Nombre', sql.VarChar(12), cliente.Nombre)
+  .input('Apellido', sql.VarChar(15), cliente.Apellido)
+  .input('NoTel', sql.VarChar(20), cliente.NoTel)
+  .input('NoDpi', sql.VarChar(20), cliente.NoDpi)
+  .execute('UpdCliente');
+  return client;
 };
 
 const getMobi = async () => {
@@ -32,8 +77,16 @@ const getPed = async () => {
 };
 
 module.exports = {
+  // Administradores
   getAdmins: getAdmins,
+
+  // Clientes
+  getClient: getClient,
   getClients: getClients,
+  insClient: insClient,
+  delClient: delClient,
+  updClient: updClient,
+
   getMobi: getMobi,
   getMueb: getMueb,
   getPed: getPed,
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const Db = require("./db/connection");
+const { json } = require("express/lib/response");
 
 /*********************/
 //   Initializations
@@ -60,7 +61,7 @@ router.route("/").get((req, res) => {
 // Admins
 router.route("/administradores").get(async (req, res) => {
   Db.getAdmins().then((dat) => {
-    console.log(dat.recordsets[0][0]);
+    console.log(dat.recordsets[0]);
     res.json({admins: dat.recordsets[0]});
   });
 });
@@ -68,9 +69,34 @@ router.route("/administradores").get(async (req, res) => {
 //Clients
 router.route("/clientes").get(async (req, res) => {
   Db.getClients().then((dat) => {
-    res.json(dat.recordsets);
+    res.json({clientes: dat.recordsets[0]});
   });
 });
+
+router.route("/clientes/:id").get(async (req, res) => {
+  Db.getClient(req.params.id).then((dat) => {
+    res.json({clientes: dat.recordsets[0]});
+  });
+});
+
+router.route("/clientes/nuevo").post( async (req, res) => {
+  Db.insClient(req.body).then(data => {
+    res.status(201).json(data);
+  }).catch(err => {console.log(err)})
+});
+
+router.route("/clientes/eliminar/:id").delete( async (req, res) => {
+  Db.delClient(req.params.id).then(data => {
+    res.status(200).json(data);
+  }).catch(err => {console.log(err)})
+});
+
+router.route("/clientes/actualizar/:id").put( async (req, res) => {
+  Db.updClient(req.params.id, req.body).then(data => {
+    res.status(200).json(data);
+  }).catch(err => {console.log(err)})
+});
+
 
 // Mob
 router.route("/mobiliario").get(async (req, res) => {
